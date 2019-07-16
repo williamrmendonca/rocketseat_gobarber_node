4f05c8a7294d2e7cbab6ad5a6ef91191f277f799
William Robson Mendonça <william@tecmasternet.com.br>
2019-07-16 17:07:39 -0300

    Ajustes na API (Retornar na Session/Controllers Avatar e provider - negar acesso de usuários q nao sejam providers)

--- src/app/controllers/ScheduleController.js
+++ src/app/controllers/ScheduleController.js
@@ -32,6 +32,13 @@ class ScheduleController {
           [Op.between]: [startOfDay(parsedDate), endOfDay(parsedDate)],
         },
       },
+      include: [
+        {
+          model: User,
+          as: 'user',
+          attributes: ['name'],
+        },
+      ],
       order: ['date'],
     });
 
--- src/app/controllers/SessionController.js
+++ src/app/controllers/SessionController.js
@@ -3,6 +3,7 @@ import * as Yup from 'yup';
 import jwt from 'jsonwebtoken'; // Importacao de modulo tem q vir antes
 
 import User from '../models/User';
+import File from '../models/File';
 
 import authConfig from '../../config/auth';
 
@@ -23,7 +24,16 @@ class SessionController {
     // Gerar Sessão de contrle de usuario (token)
     const { email, password } = req.body;
     // Verifica se email existe
-    const user = await User.findOne({ where: { email } });
+    const user = await User.findOne({
+      where: { email },
+      include: [
+        {
+          model: File,
+          as: 'avatar',
+          attributes: ['id', 'path', 'url'],
+        },
+      ],
+    });
     if (!user) {
       return res.status(400).json({ error: 'User not found.' });
     }
@@ -33,12 +43,15 @@ class SessionController {
       return res.status(400).json({ error: 'Password does not mach.' });
     }
 
-    const { id, name } = user;
+    const { id, name, avatar, provider } = user;
+
     return res.json({
       user: {
         id,
         name,
         email,
+        provider,
+        avatar,
       },
       token: jwt.sign({ id }, authConfig.secret, {
         expiresIn: authConfig.expiresIn,
